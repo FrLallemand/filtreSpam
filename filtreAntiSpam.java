ef99f6a1bc430dea14603201124e26cad7712332
François Lallemand <francoislallemand@francoislallemand-linux.home>
2018-04-26 16:01:52 +0200

    ajout de lire_message

--- filtreAntiSpam.java
+++ filtreAntiSpam.java
@@ -3,7 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class filtreAntiSpam {
 
@@ -24,7 +25,7 @@ public class filtreAntiSpam {
 
 			String line;
 			while((line = br.readLine()) != null){
-				if(line.length > 3){
+				if(line.length() >= 3){
 					this.dictionnaire.add(line);
 				}
 			}
@@ -37,9 +38,35 @@ public class filtreAntiSpam {
 	/*
 	  Fonction lire_message : cette fonction doit pouvoir lire un message (dans un fichier texte) et le
 	  traduire en une représentation sous forme de vecteur binaire x à partir d’un dictionnaire.
-	*/
-	public void lire_message(){
 
+	  Seule la présence des mots est prise en compte, par le nombre d'occurences.
+	*/
+	public boolean[] lire_message(String path){
+		// false par défaut
+		boolean[] vecteur = new boolean[this.dictionnaire.size()];
+
+		try{
+			InputStream input = new FileInputStream(path);
+			BufferedReader br=new BufferedReader(new InputStreamReader(input));
+
+			String line;
+			while((line = br.readLine()) != null){
+				String[] words = line.split("\\W+");
+				for(String word : words){
+					if(word.length() >= 3){
+						word = word.toUpperCase();
+						if(this.dictionnaire.contains(word)){
+							vecteur[this.dictionnaire.indexOf(word)] = true;
+						}
+					}
+				}
+			}
+			br.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return vecteur;
 	}
 
 	public static void main(String[] args) {
